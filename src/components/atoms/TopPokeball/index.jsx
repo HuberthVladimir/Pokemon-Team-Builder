f4bf6e61072b3aeaf6cca1cf661152a79e7c7ef0
Huberth <vladimirhuberth@hotmail.com>
2021-09-10 19:01:15 -0300

    style: top pokeball style changed

--- src/components/atoms/TopPokeball/index.jsx
+++ src/components/atoms/TopPokeball/index.jsx
@@ -1,8 +1,7 @@
-import Topball from '../../../assets/topPokeball.svg'
 import './style.scss'
 
 export const TopPokeball = (props) => {
    return (
-      <img className={`topPokeballFilter ${props.className}`} src={Topball} alt="pokeball superior" />
+      <div className={`topPokeballFilter ${props.className}`} />
    )
 }
