7afcc5a2e75aba17161b51befb30c8bbe95ddfd2
Huberth <vladimirhuberth@hotmail.com>
2021-09-12 02:33:04 -0300

    feat: get Pokemons chosed added

--- src/components/organisms/PokemonCard/index.jsx
+++ src/components/organisms/PokemonCard/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.scss'
 import { Circle } from '../../atoms/circle'
 import { Pokemon } from '../../atoms/pokemon'
 import { PokemonName } from '../../atoms/pokemonName'
 import { BoxTypeColor } from '../../molecules/boxTypeColor'
+import UseAppGlobalContext from '../../../services/context'
 
 export const PokemonCard = ({ id, image, name, firstType, secondType }) => {
+   const { savedPokemons } = useContext(UseAppGlobalContext)
+
+   const handleCardClick = () => {
+      const alreadyExist = savedPokemons.map(data => data.name.includes(name))
+      const objPokemon = { id, name, firstType }
+
+      if (alreadyExist.includes(true)) {
+         alert('You arealdy take that pokemon')
+      } else if (savedPokemons.length >= 6) {
+         alert(`You party it's alrady full`)
+      } else {
+         savedPokemons.push(objPokemon)
+      }
+   }
+
    return (
-      <div className="pokemonCard">
+      <div className="pokemonCard"
+         onClick={handleCardClick}
+      >
          <Circle>{id}</Circle>
          <Pokemon image={image} />
          <PokemonName>{name.indexOf('-') > -1 ? name.substring(0, name.indexOf("-")) : name}</PokemonName>
--- src/services/context.jsx
+++ src/services/context.jsx
@@ -1,16 +1,18 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 
 const UseAppGlobalContext = createContext({})
 
 export const UseAppProvider = ({ children }) => {
    const [isInputTeamActive, setIsInputTeamActive] = useState(false)
    const [inputText, setInputText] = useState('My team')
+   const [savedPokemons, setSavedPokemons] = useState([])
 
    return (
       <UseAppGlobalContext.Provider
          value={{
             isInputTeamActive, setIsInputTeamActive,
-            inputText, setInputText
+            inputText, setInputText,
+            savedPokemons, setSavedPokemons
          }}>
          {children}
       </UseAppGlobalContext.Provider>
